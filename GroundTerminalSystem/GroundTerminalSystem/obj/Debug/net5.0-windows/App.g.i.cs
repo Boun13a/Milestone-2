6eb2fe3c31698cb4d376ad1f89feab6868bf8d86
lantanakamara <asadatnish@gmail.com>
2021-11-12 14:49:37 -0500

    added the class that writes what comes back from the search in the database to a text file

--- GroundTerminalSystem/GroundTerminalSystem/obj/Debug/net5.0-windows/App.g.i.cs
+++ GroundTerminalSystem/GroundTerminalSystem/obj/Debug/net5.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace GroundTerminalSystem {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.4.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.11.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace GroundTerminalSystem {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.4.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.11.0")]
         public static void Main() {
             GroundTerminalSystem.App app = new GroundTerminalSystem.App();
             app.InitializeComponent();
