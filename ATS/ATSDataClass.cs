7287ec9557f6a09c46f0d18a12c4e96c0cf5ce1d
Halbouni <Mhalbouni8919@conestogac.on.ca>
2021-11-11 22:10:17 -0500

    Adding ATSDataClass.cs class which contains all the private data members for all other classes

---
+++ ATS/ATSDataClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading;
+
+namespace AirTransmissionSystem
+{
+    class ATSDataClass
+    {
+        private string rawString;
+        private string fileName;
+        private string line;
+        private string stringToSend;
+        private string airCraftTail;
+        private uint sequenceNumber;
+        private int checkSum;
+
+
+        public uint SequenceNumber{ get; set; }
+        public int CheckSum { get; set; }
+        public string AirCraftTail { get; set; }
+        public string StringToSend{ get; set; }
+        public string Line{ get; set; }
+        public string FileName { get; set; }
+        public string RawString { get; set; }
+    }
+}
