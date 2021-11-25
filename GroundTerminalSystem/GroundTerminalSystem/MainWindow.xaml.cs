33b50db18d00e85ed5fb14e1e6c18d8b56f016ac
Mohammed Abusultan <Mabusultan5425@conestogac.on.ca>
2021-11-25 14:40:14 -0500

    Added Comments

--- GroundTerminalSystem/GroundTerminalSystem/MainWindow.xaml.cs
+++ GroundTerminalSystem/GroundTerminalSystem/MainWindow.xaml.cs
@@ -1,7 +1,24 @@
-﻿using System;
+﻿/*
+* FILE : GroundTerminalSystem
+* Authors: Asad Ahmed, Mohammed Abusultan, Mohamed Halbouni
+* DESCRIPTION : This file contains The program that simulates the ground terminal system
+* of the Air Transportation System. This program receives telemetry data from the Air Transmission
+* System or the simulated plane that sends data every 1 second.
+* The GroundTerminalSytem, displays the telemetry database on the UI in realtime and also 
+* persistes the data to the database
+* this includes opening and closing of the database connections as well as inserting
+* telemetry database into their respective talbles
+*/
+
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,6 +29,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
 
 namespace GroundTerminalSystem
 {
@@ -22,10 +40,15 @@ namespace GroundTerminalSystem
     {
         public MainWindow()
         {
+
             InitializeComponent();
+            
+            //Thread thStart = new Thread(startServer);
+            //thStart.Start();
         }
 
-        
+
+
         private void HandleCheck(object sender, RoutedEventArgs e)
         {
             
