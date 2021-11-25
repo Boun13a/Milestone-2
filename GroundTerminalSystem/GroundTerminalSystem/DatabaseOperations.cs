55632501b50443f9bb646b02d1524dbc85766d59
lantanakamara <asadatnish@gmail.com>
2021-11-25 13:59:40 -0500

    created the class that will handle database operations

---
+++ GroundTerminalSystem/GroundTerminalSystem/DatabaseOperations.cs
@@ -0,0 +1,47 @@
+﻿/*
+* Class : DatabaseOperations.cs
+* DESCRIPTION : This file contains the class that manages the database operations
+* this includes opening and closing of the database connections as well as inserting
+* telemetry database into their respective talbles
+*/
+
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace GroundTerminalSystem
+{
+    class DatabaseOperations
+    {
+        private MySqlConnection cnn;
+
+        /*
+         * Method Name: ConnectToDatabase()
+         * Description: This method opens the connection to the database using SQL connection
+         * string
+         * Parameters: string connectionString
+         * Return value : void
+         */
+
+        public MySqlConnection ConnectToDatabase(string connectionString)
+        {
+
+            try
+            {
+                cnn = new MySqlConnection(connectionString);
+
+
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+                cnn.Close();
+            }
+            return cnn;
+        }
+    }
+}
--- GroundTerminalSystem/GroundTerminalSystem/obj/Debug/net5.0-windows/MainWindow.g.i.cs
+++ GroundTerminalSystem/GroundTerminalSystem/obj/Debug/net5.0-windows/MainWindow.g.i.cs
@@ -77,7 +77,7 @@ namespace GroundTerminalSystem {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/GroundTerminalSystem;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/GroundTerminalSystem;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
