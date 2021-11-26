0a7452e2327bffdce962c1404c74688cf30915a8
Halbouni <Mhalbouni8919@conestogac.on.ca>
2021-11-26 16:16:59 -0500

    Adding the read from file function

--- ATS/ReadFromFile.cs
+++ ATS/ReadFromFile.cs
@@ -37,6 +37,103 @@ namespace AirTransmissionSystem
         }
 
 
+
+		//FUNCTION NAME				:				public void ReadData(int argLineNumbers,string argFileName)
+		//PARAMETERS				:				string argFileName - the txt file name to read line from.
+		//											int argLineNumbers - contains the total lines in each of the file.
+		//DESCRIPTION				:				This function is to read from text file and if it's the end of the file it's going to abort the thread
+		//											that is reading from the corresponding file. mutex is implemented in order to control each resource
+		//											and have a 1 second delay for every line read.
+		//RETURN					:				NONE
+        public void ReadData(int argLineNumbers,string argFileName)
+        {
+            int i = 0;
+
+            if (Thread.CurrentThread.Name == "C-FGAX")
+            {
+
+                var path = Path.Combine(Directory.GetCurrentDirectory(), argFileName);						//get the txt file path dinamically
+                using (StreamReader dataFile = new StreamReader(path))
+                {
+
+                    for (i = 0; i < argLineNumbers; i++)					//check if it is less than the total lines in C-FGAX.txt
+                    {
+                        if (i == argLineNumbers)
+                        {
+                            Thread.CurrentThread.Abort();					//aborth this thread if its = total lines.
+                        }
+                        else
+                        {
+                            lockThread.WaitOne();							//lock this resource for this thread
+                            data.SequenceNumber += 1;						//To increment the sequence number.
+                            data.Line = dataFile.ReadLine();				//Read the line from the txt file
+                            data.StringToSend = format.StringToFormat(data.Line, Thread.CurrentThread.Name,(int)(data.SequenceNumber));				//format the string
+                            sendLine.SendLines(data.StringToSend);				//send the formatted string to the send function to be sent to the server.
+                            Thread.Sleep(1000);             					//sleep 1 second
+                            lockThread.ReleaseMutex();							//release the mutex
+                        }
+                    }
+                }
+            }
+
+            if (Thread.CurrentThread.Name == "C-GEFC")
+            {
+                data.Line = Thread.CurrentThread.Name + ".txt";
+                var path = Path.Combine(Directory.GetCurrentDirectory(), argFileName);
+                using (StreamReader dataFile = new StreamReader(path))
+                {
+
+                    for (i = 0; i < argLineNumbers; i++)
+                    {
+                        if (i == argLineNumbers)							//check if it is less than the total lines in C-GEFC.txt
+                        {
+                            Thread.CurrentThread.Abort();					//aborth this thread if its = total lines.
+                        }
+                        else
+                        {
+                            lockThread.WaitOne();							//lock this resource for this thread
+                            data.SequenceNumber += 1;						//To increment the sequence number.
+                            data.Line = dataFile.ReadLine();				//Read the line from the txt file
+                            data.StringToSend = format.StringToFormat(data.Line, Thread.CurrentThread.Name, (int)(data.SequenceNumber));			//format the string
+                            sendLine.SendLines(data.StringToSend);				//send the formatted string to the send function to be sent to the server.
+                            Thread.Sleep(1000);            						//sleep 1 second
+                            lockThread.ReleaseMutex();							//release the mutex
+                        }
+                    }
+                }
+            }
+
+            if (Thread.CurrentThread.Name == "C-QWWT")
+            {
+                data.Line = Thread.CurrentThread.Name + ".txt";
+                var path = Path.Combine(Directory.GetCurrentDirectory(), argFileName);
+                using (StreamReader dataFile = new StreamReader(path))
+                {
+
+                    for (i = 0; i < argLineNumbers; i++)
+                    {
+                        if (i == argLineNumbers)							//check if it is less than the total lines in C-GEFC.txt
+                        {
+                            Thread.CurrentThread.Abort();					//aborth this thread if its = total lines.
+                        }
+                        else
+                        {
+                            lockThread.WaitOne();							//lock this resource for this thread
+                            data.SequenceNumber += 1;						//To increment the sequence number.
+                            data.Line = dataFile.ReadLine();				//Read the line from the txt file
+                            data.StringToSend = format.StringToFormat(data.Line, Thread.CurrentThread.Name, (int)(data.SequenceNumber));			//format the string
+                            sendLine.SendLines(data.StringToSend);				//send the formatted string to the send function to be sent to the server.
+                            Thread.Sleep(1000);             					//sleep 1 second
+                            lockThread.ReleaseMutex();							//release the mutex
+                        }
+                    }
+                }
+            }
+           
+        }
+	
+
+
 		//FUNCTION NAME				:				public int CountLines(string argFilePath)
 		//PARAMETERS				:				string argFilePath - the txt file to count the number of lines from.
 		//DESCRIPTION				:				This function is to count how many lines in each of the txt file.
